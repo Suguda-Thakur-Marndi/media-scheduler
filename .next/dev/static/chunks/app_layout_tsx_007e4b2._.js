83c25d6a715912c1d921bfb28186e0e250bd59f3
Suguda-Thakur-Marndi <sugudathakurmarndi1106@gmail.com>
2026-06-16 20:18:28 +0530

    "first"

--- .next/dev/static/chunks/app_layout_tsx_007e4b2._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_@clerk_nextjs_dist_esm_app-router_1h752i4._.js","static/chunks/node_modules_@clerk_shared_dist_1unvqcj._.js","static/chunks/node_modules_@tanstack_query-core_build_modern_16fcf6f._.js","static/chunks/node_modules_@clerk_react_dist_0tqbzaq._.js","static/chunks/node_modules_next_1nbxz3v._.js","static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_0q79z23._.js","static/chunks/node_modules_1den4q6._.js","static/chunks/_0g05l3n._.js","static/chunks/[root-of-the-server]__0cbk-n2._.css"],
-    source: "dynamic"
-});
--- app/routes/dashboard/ideas/page.tsx
+++
@@ -1,9 +0,0 @@
-import IdeaKanban from "@/components/idea/idea-kanban"
-
-const IdeasPage = () => {
-  return (
-    <IdeaKanban />
-  )
-}
-
-export default IdeasPage
